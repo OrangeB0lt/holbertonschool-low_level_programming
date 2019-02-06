e916c91ff3782ec393743885dd3212f7e4632850
OrangeB0lt <667@holbertonschool.com>
2019-02-06 17:25:58 +0000

    added space betwee return and 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-/*
+/**
  * main is the funtion the program runs
+ *
+ *RETURN 0 TO EXIT PROGRAM
  */
 int main(void) {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return(0);
-}
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+} 
