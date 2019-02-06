7cbc90a9bc2b8b589b54f37e038a1ea31471ff7d
OrangeB0lt <667@holbertonschool.com>
2019-02-06 20:32:23 +0000

    First upload of odd way to print out quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,14 @@
 /**
  * main - prints quote using write function
  *
- * Return: 0
+ * Return: 0 to exit program
  **/
 
 int main(void)
 {
 	int i = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19/n");
+
 	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", i);
-	return(0);
+
+	return (0);
 }
