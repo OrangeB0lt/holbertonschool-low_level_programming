75bc89252a8d923795f6ef5d90109b2762ac21e6
OrangeB0lt <667@holbertonschool.com>
2019-02-06 19:05:33 +0000

    Fixed 6-size.c removed a space and did some formatting

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,14 @@
 /**
  * main - funtion prints out byte sizes of different variable types
  *
+ * Description: prints the size of char, int, lont int, long long int, and float
  * Return: 0 to exit program
  */
 
 int main(void)
 {
 	printf("Size of a char; %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof (int));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
