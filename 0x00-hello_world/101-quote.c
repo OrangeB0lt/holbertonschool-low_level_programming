1f69a9af01f8c9cb19eea337364c04e0ab1c78d3
OrangeB0lt <667@holbertonschool.com>
2019-02-06 21:52:19 +0000

    Removed I manually put in 59 characters

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,10 @@
 
 int main(void)
 {
-	int i = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", i);
+/*
+ *int i = strlen("and that piece of art is useful\" - Dora Korpar, 2015-
+ */
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
