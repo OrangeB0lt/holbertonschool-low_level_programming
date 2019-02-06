240d91eee015fc20ab46ae1faf33356e28cf9b8a
OrangeB0lt <667@holbertonschool.com>
2019-02-06 17:38:30 +0000

    fixed tabs before puts and return to be actual tabs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  *RETURN 0 TO EXIT PROGRAM
  */
 int main(void) {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 } 
