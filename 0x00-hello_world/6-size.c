724dd94862cdd28ff4dbe2d8c8a9a659a7e2f825
OrangeB0lt <667@holbertonschool.com>
2019-02-06 19:12:40 +0000

    Fixed 6-size.c removed a space and did some formatting

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	printf("Size of a char; %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a char; %zd byte(s)\n", sizeof(char));
+	printf("Size of an int: %zd byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zd byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %zd byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zd byte(s)\n", sizeof(float));
 	return (0);
 
 }
