39e0d9a94d7dbf5da3187d20fe3ebf00ea8bd97f
OrangeB0lt <667@holbertonschool.com>
2019-02-12 12:51:21 -0500

    First upload of print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 -   prints the alphabet in all lower case 10 times
+ *
+ * Description: prints alphabet in all lowercase, prints new line
+**/
+
+void print_alphabet_x10(void);
+{
+	char i = 'a';
+	int n;
+
+	for (n = 0 ; n <10 ; n++)
+	{
+		while (i <= 'z')
+		{
+			_putchar(i++);
+		}
+	_putchar('\n');
+	}
+}
