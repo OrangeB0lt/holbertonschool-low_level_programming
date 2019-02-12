880ce26eb8f0fc86de5ed7c3127679a57d1dbc36
OrangeB0lt <667@holbertonschool.com>
2019-02-12 13:51:00 -0500

    Upload of holberton.h and function 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _islower - returns something depending on the case of the letter
+ *
+ * Return: 1 if lower case, otherwise returns 0
+ **/
+
+int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}else{
+		return (0);
+
+	}
+}
