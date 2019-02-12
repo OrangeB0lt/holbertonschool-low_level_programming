a84d72103d141d2bfe41f29f0f6f291d7ff9f5eb
OrangeB0lt <667@holbertonschool.com>
2019-02-12 12:01:18 -0500

    Function that writes the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_alpabet - uses a forloop to print the alphabet in all lower case
+ **/
+
+int print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
