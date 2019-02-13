f0c0aa1c9c699b08adb145ca1bd8a73a27839fbd
OrangeB0lt <667@holbertonschool.com>
2019-02-13 02:36:04 +0000

    First upload of 8-24 hours, prints time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day in military time
+ **/
+
+void jack_bauer(void)
+{
+	int i = 0;
+	int x = 0;
+
+	while (i < 24)
+	{
+		while (x < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar('\n');
+			x++
+
+		}
+		i++
+	      	x = 0;
+	}
+}
