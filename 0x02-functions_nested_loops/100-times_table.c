e3e521e3a50929c4cd2321e93a4d7fc2be14bcb2
OrangeB0lt <667@holbertonschool.com>
2019-02-13 03:20:10 +0000

    First upload of times tables

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - print times tables from zero to n
+ * @n: integer
+ **/
+
+void print_times_table(int n)
+{
+	int i = 0;
+
+	if (n >= 0 && n <= 15)
+	{
+		while (i <= n)
+		{
+			int x = 1;
+
+			_putchar('0');
+			while (x <= n)
+			{
+				int o = i * x;
+
+				_putchar(',');
+				_putchar(' ');
+				if (o < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(o + '0');
+				}
+				else if (o > 99)
+				{
+					_putchar((o / 100) + '0');
+					_putchar(((o / 10) % 10) + '0');
+					_putchar((o % 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar((o / 10) + '0');
+					_putchar((o % 10) + '0');
+				}
+				x++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+}
