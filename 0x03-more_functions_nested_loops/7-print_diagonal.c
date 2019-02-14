1e50362e0700a9028b67f44df086b54ac9b502fe
OrangeB0lt <667@holbertonschool.com>
2019-02-14 17:46:16 -0500

    first upload of 7 print a diagonal

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "hoblerton.h"
+
+/**
+* print_diagonal - the function prints \ in a diagonal pattern
+*
+* @n: size of diagonal
+*
+* Return: none
+**/
+
+void print_diagonal(int n);
+{
+	int i, x;
+
+	for (i = 0; i < n; i++)
+	{
+		for (x = 0; x < n; n++)
+		{
+			if (x != i)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(\\);
+			}
+		}
+		_putchar('\n')
+	}
+	if (n <= 0)
+	   _putchar('\n');
+}
