a57c58fb0c9a17369b7f11ab6c5e7d9d84dcea8f
OrangeB0lt <667@holbertonschool.com>
2019-02-14 15:11:17 -0500

    First upload of mul, multiplies two variables together

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies the two input variables
+ * @a: variable one
+ * @b: variable two
+ *
+ * Return: returns a and b multiplied together
+ **/
+
+int mul(int a, int b)
+{
+	return (a * b)
+}
