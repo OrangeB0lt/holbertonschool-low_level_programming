334965ef97bbfa2e5f00dca5b741d260798a1c07
OrangeB0lt <667@holbertonschool.com>
2019-02-14 14:40:46 -0500

    First commit for isupper

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /**
  * _isupper - returns a 1 or 0 depending on if the letter is uppercase or lower
- * @c; C is the letter the function is checking
+ * @c: C is the letter the function is checking
  *
  * Return: return 1 is uppercase or 0 if lower
  *
