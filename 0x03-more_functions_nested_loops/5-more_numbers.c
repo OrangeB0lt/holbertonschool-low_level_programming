ae54d55db0dc6c75b7cecb185755e1e5fe8342f4
OrangeB0lt <667@holbertonschool.com>
2019-02-14 15:50:41 -0500

    First upload of more_numbers

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints numbers ranging fro 0 to 14 10x
+ *
+ * Return: no return
+ *
+ *
+ **/
+
+void more_numbers(void)
+{
+	int i, ;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+	}
+
+}
