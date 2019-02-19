ecd518d927202950b958edcedaaae0c936ca5fe4
OrangeB0lt <667@holbertonschool.com>
2019-02-19 13:26:03 -0500

    Removed extra character

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -4,7 +4,7 @@
  * _strcpy - copies a string to a destination
  * @dest: destination of string
  * @src: source string to copy
- * Return: points tot he destination
+ * Return: points to the destination
  **/
 
 char *_strcpy(char *dest, char *src)
@@ -15,7 +15,5 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-
-	dest[i] = '\0';
 	return (dest);
 }
