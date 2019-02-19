03fafc1435f14615d4cd00e2dcb542f0bc47d0aa
OrangeB0lt <667@holbertonschool.com>
2019-02-19 11:16:07 -0500

    First upload of function to find string length

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of the string
+ *@s: characters in string
+ * Return: returns length of string
+ **/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[1] != 0; i++)
+	{
+	}
+	return (i);
+}
