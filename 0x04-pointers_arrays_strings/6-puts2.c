57ee0990e3a3458c66c869e7749998851655db08
OrangeB0lt <667@holbertonschool.com>
2019-02-19 12:09:15 -0500

    First commit of function to print every other character

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * puts2 - prints out every other character in a string
+ * @str: string printing out
+ *
+ **/
+
+void puts2(char *str);
+{
+	int i;
+	char x;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (i % 2 == 0)
+		{
+			x = str[i];
+			_putchar(x);
+		}
+	}
+	_putchar('\n');
+}
