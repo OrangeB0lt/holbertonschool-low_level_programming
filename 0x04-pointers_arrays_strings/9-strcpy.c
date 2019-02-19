7df4e4eee19c614e4c16aed5e9747b6707e0fc53
OrangeB0lt <667@holbertonschool.com>
2019-02-19 12:42:52 -0500

    First upload of string copy function

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copies a string to a destination
+ * @dest: destination of string
+ * @src: source string to copy
+ * Return: points tot he destination
+ **/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[i] = src [i];
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
