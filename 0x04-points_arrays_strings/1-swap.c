29abcd8abcdd1215210af5f66861051d7f393bb7
OrangeB0lt <667@holbertonschool.com>
2019-02-19 11:00:34 -0500

    First submit for swapping integers

---
+++ 0x04-points_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps places of integers
+ * @a: first integer
+ * @b: second integer
+ *
+ **/
+
+void swap_int(int *a, int *b);
+{
+	int n;
+
+	n = *a;
+	*a = *b;
+	*b = n;
+}
