9d83f5eb8b8be1c6beaa6777e5a00145675b1db5
OrangeB0lt <667@holbertonschool.com>
2019-02-21 13:04:10 -0500

    First commit of string copy

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#included "holberton.h"
+
+/**
+ * _strncpy -  copies a string from the source to the destination string
+ * @n: number of chars
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: returns destination
+ **/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i != n)
+	{
+		dest[i] = src[i];
+		if (src[i] == '\0')
+		{
+			while (i != n)
+			{
+				dest(i++) = '\0';
+			}
+			break;
+		}
+		i++;
+	}
+	return (dest);
+}
