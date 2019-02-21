93d0997a3ed9b987fc9a1046a2017168e845067a
OrangeB0lt <667@holbertonschool.com>
2019-02-21 12:42:17 -0500

    First commit of strncat but for strings of n size

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strncat - concatenates strings, with given values of characters
+ *
+ * @n: num of chars to conatenate
+ * @dest: destination string
+ * @src: source string
+ * Return: returns destination string
+ *
+ **/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, x;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	}
+	x = 0;
+	while (src [x] != '\0' && x != n)
+	{
+		dest[i++] = src [x++];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
