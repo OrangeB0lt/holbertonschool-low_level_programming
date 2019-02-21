24a4fb3b705867901e8d736706a8a1f8b4206b06
OrangeB0lt <667@holbertonschool.com>
2019-02-21 13:32:43 -0500

    Fixed layout for betty format

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * _strcmp - function to compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: returns string 1 - string 2 or 0
  **/
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
