14c8224a63ab28c2a0ca780e1d74522c5fcbdae8
OrangeB0lt <667@holbertonschool.com>
2019-02-21 20:42:27 -0500

    Removed else statement

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -19,15 +19,12 @@ char *rot13(char *n)
 		{
 			n[i] += 13;
 		}
-		else
+		while ((n[i] >= 'N' && n[i] <= 'Z') ||
+		       (n[i] >= 'n' && n[i] <= 'z'))
 		{
-			while ((n[i] >= 'N' && n[i] <= 'Z') ||
-			       (n[i] >= 'n' && n[i] <= 'z'))
-			{
 			n[i] -= 13;
-			}
 		}
-		i++;
 	}
+	i++;
 	return (n);
 }
