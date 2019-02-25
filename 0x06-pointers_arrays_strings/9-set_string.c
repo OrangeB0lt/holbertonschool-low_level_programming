addcf3683519a36131bc6af56ec8a90ad0e7ca42
OrangeB0lt <667@holbertonschool.com>
2019-02-25 18:10:48 -0500

    First commit of double pointer fun

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of the pointer to char
+ * @s: pointer to set
+ * @to: pointer to destination
+ * Return: none
+ **/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
