cee1885ab992976c1d3b59e0e664d37659fc08e4
OrangeB0lt <667@holbertonschool.com>
2019-02-25 17:16:07 -0500

    First commit for string to find set of bytes

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - searches string for any sets of bytes
+ * @s: string
+ * @accept: string compared to string
+ * Return: returns null if not matching
+ **/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (s[i] == accept[n])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return('\0')
+}
