a77cfe6b7d08d4b2c5f3de53d58ee18ed20fb3d1
OrangeB0lt <667@holbertonschool.com>
2019-02-25 13:55:47 -0500

    First uplaod for memset function

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _memset - main function to print memory sets
+ * @s: string
+ * @b: constant byte
+ * @n: unsigned integer
+ **/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
