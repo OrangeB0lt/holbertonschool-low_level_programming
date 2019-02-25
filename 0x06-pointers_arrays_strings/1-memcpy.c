2d3bf409b00796995c1af1294346f791d230fa4d
OrangeB0lt <667@holbertonschool.com>
2019-02-25 14:29:19 -0500

    first upload of mem copy function

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * _memcpy - copys bit of memory
+ * @dest: destination array
+ * @src: source array
+ * @n: oh many bytes to copy
+ * Return: returns destination string
  **/
 
 char *_memcpy(char *dest, char *src, unsigned int n)
