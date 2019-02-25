db5ec39795f305475ae5f7f40a2b829d7f47259f
OrangeB0lt <667@holbertonschool.com>
2019-02-25 18:04:43 -0500

    First commit of diagsum, adding two diagonal numbers

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums - prints the sum of two diagonal ints in a matrix
+ * @a: array
+ * @size: size of array
+ **/
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size * size; i += (size + 1))
+		s1 += a[i];
+	for (i = size - 1; i < (size * size) - (size - 1); i += (size - 1))
+		s2 += a[i];
+	printf("%d, %d\n", sum1, sum2);
+
+}
