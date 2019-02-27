5d1974bb59486bbab0de8fb8a907761cd6c0c576
OrangeB0lt <667@holbertonschool.com>
2019-02-27 18:18:32 -0500

    First commit of print string rev

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints the input string in reverse
+ * @s: the string
+ **/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
