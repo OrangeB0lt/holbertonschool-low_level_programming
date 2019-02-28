c16584f92cb920647cc789363350ee9ebb03e3fe
OrangeB0lt <667@holbertonschool.com>
2019-02-27 20:24:48 -0500

    First commit of prime check program

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - checks if value is prime
+ * @n: input value
+ * Return: returns booleen value
+ **/
+
+int is_prime_number(int n)
+{
+	return (prime_check(n, 2));
+}
+
+/**
+ * prime_check - checks if value is prime
+ * @i: input value
+ * @n: divisor
+ * Return: returns a boolean statement
+ **/
+
+int prime_check(int i, int n)
+{
+	if (i < 2)
+	{
+		return (0);
+	}
+	else if (n * n > i)
+	{
+		return (1);
+	}
+	return (i % n == 0 ? 0 : (prime_check(i, n + 1)));
+}
