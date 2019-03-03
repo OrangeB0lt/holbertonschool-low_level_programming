db83f22c4f5421641fa1a4b2ccb96780ee501b47
OrangeB0lt <667@holbertonschool.com>
2019-03-03 14:08:10 -0500

    First commit of program that counts the number of arguments coming in

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * main - prints number of arguments coming into the program
+ * @argc: number of arguments
+ * @argv: arguments coming in
+ * Return: returns 0 to end program
  **/
 
 int main(int argc, char *argv[])
