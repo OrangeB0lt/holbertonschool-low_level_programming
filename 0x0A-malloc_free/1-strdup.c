010b85d2b2d192ae69206295b7304e62823a278b
OrangeB0lt <667@holbertonschool.com>
2019-03-05 16:11:05 -0500

    First commit of string duplicate

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a duplicate string
+ * @str: string to duplicate
+ * Return: returns copy of a pointer or NULL
+ **/
+
+char *_strdup(char *str)
+{
+	char *cpy;
+	unsigned int i, n;
+
+	if (str == NULL)
+	{
+		return(NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	cpy = malloc(++i * sizeof(char));
+	if (cpy == NULL)
+	{
+		return (NULL);
+	}
+	for (n = 0; n < i; n++)
+	{
+		cpy[n] = str [n];
+	}
+	return (cpy);
+}
