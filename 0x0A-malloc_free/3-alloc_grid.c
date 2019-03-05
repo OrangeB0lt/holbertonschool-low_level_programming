7f5b716c98db313f73751ce1cc6ff3d9c68258c1
OrangeB0lt <667@holbertonschool.com>
2019-03-05 18:50:52 -0500

    First commit of square memory allocation

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 		a[i] = malloc(sizeof(int) * width);
 		if (a[i] == NULL)
 		{
-			for (--i; i>= 0; i--)
+			for (--i; i >= 0; i--)
 			{
 				free(a[i]);
 			}
