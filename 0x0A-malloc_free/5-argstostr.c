e7e12d0da8534da9622a5a88f2db7cc4ad36a3c8
OrangeB0lt <667@holbertonschool.com>
2019-03-05 19:25:32 -0500

    First commit of function to concat arguments put into the program

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ **/
+
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int i, n, x, l;
+
+	i = n = x = l = 0;
+
+	if (av == 0 || ac == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < ac; l++, i++)
+	{
+		for (n = 0; av[i][n] != 0; l++, n++)
+		{
+		}
+	}
+	l++;
+	s = (char *)malloc(sizeof(char) * l);
+	if (s == 0)
+	{
+		free(s);
+		return (0);
+	}
+	i = 0;
+	for (i = 0; i < ac; i++, x++)
+	{
+		for (n = 0; av[i][n] != 0; n++)
+		{
+			s[x] = av[i][n], x++;
+		}
+		s[x] = '\n';
+	}
+	s[x] = 0;
+	return (s);
+}
