5dddda39352dd05cd2e8a44e7f4ad7c5d962d0dd
OrangeB0lt <667@holbertonschool.com>
2019-03-07 12:38:55 -0500

    fixed malloc statement

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -32,12 +32,3 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	pointer[i] = '\0';
 	return (pointer);
 }
-int main(void)
-{
-	char *concat;
-
-	concat = string_nconcat("Holberton ", "School !!!", 6);
-	printf("%s\n", concat);
-	free(concat);
-	return (0);
-}
