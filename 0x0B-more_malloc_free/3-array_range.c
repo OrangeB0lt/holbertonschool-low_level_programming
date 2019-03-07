b4ac338085189c2b67007e51f1c33327b6277845
OrangeB0lt <667@holbertonschool.com>
2019-03-07 13:46:48 -0500

    First commit for making an aray from min to max size

---
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+ * array_range - makes an array from min to max
+ * @min: min number
+ * @max: max number
+ * Return: returns the pointer to the array, or NULL if malloc fails
+
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i;
+
+	if (min > max)
+		return (NULL);
+	arr = malloc(((max - min) + 1) * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+	i = 0;
+	while (min <= man)
+		arr[i++] = min++;
+	return (arr);
+}
