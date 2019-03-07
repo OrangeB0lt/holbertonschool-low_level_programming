a2252ef3b966a014eeaa91d1776926e0f9c5e728
OrangeB0lt <667@holbertonschool.com>
2019-03-07 13:23:51 -0500

    Fixed up comments and misspelt vars

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 
 /**
+ * _mem - sets the first n bytes of the mem
+ * @string: array to set
+ * @num: number to set
+ * @n: first n of bytes
+ * Return: returns string
+ **/
+
+char _mem(char *string, char num, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		string[i] = num;
+	return (string);
+}
+
+/**
  * _calloc - uses malloc to assign memory, then sets it to zero
  * @nmemb: memory spaces
  * @size: size of nmemb
@@ -10,21 +27,13 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
-	char *allocate;
+	void *pointer;
 
-	if (nmemb == 0 || size == 0)
-	{
+	if (nmem == 0 || size == 0)
 		return (NULL);
-	}
-	allocate = malloc(nmemb * size);
-	if (allocate == NULL)
-	{
+	pointer = malloc(nmemb * size);
+	if (pointer == NULL)
 		return (NULL);
-	}
-	for (i = 0; i != size; i++)
-	{
-		*(allocate + (size * i)) = 0;
-	}
-	return (allocate);
+	_mem(pointer, 0, (nmemb * size));
+	return (pointer);
 }
