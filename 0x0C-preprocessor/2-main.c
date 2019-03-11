2ed60b1c0c2aea61cff135701d5b05639c43e818
OrangeB0lt <667@holbertonschool.com>
2019-03-11 13:21:04 -0400

    First commit of programto print out header file names

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#ifndef __FILE__
+#define __FILE__
+#endif
+
+/**
+ * main - prints file name then a new line
+ * Return: 0
+ **/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
