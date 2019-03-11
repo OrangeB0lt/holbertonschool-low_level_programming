c5a56e4ebdaf6abc89f0beb39cdb68a79269ce40
OrangeB0lt <667@holbertonschool.com>
2019-03-11 17:32:36 -0400

    First commit for printing dog structure

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -8,7 +8,7 @@
 
 void print_dog(struct dog *d)
 {
-	if (d== NULL)
+	if (d == NULL)
 	{
 		return;
 	}
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 		printf("Name; (nil)\n");
 	}
 	else
-		printf ("Name: %s\n", d->name);
+		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 	{
