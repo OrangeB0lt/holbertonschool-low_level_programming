0d5d6080d46beb62a576303e8e63d0c13ac2146b
OrangeB0lt <667@holbertonschool.com>
2019-03-11 22:08:08 -0400

    First commit of new dog file

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -10,11 +10,11 @@
  * Return: returns new_dog
  **/
 
-dog_ t *new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new_dog;
 
-	new_dog = malloc(sizeof(stuct dog));
+	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 	{
 		return (NULL);
