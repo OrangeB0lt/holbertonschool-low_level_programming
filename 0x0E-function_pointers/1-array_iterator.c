9bd646c23512e42d1cad102619bff7f8a4edd25c
OrangeB0lt <667@holbertonschool.com>
2019-03-13 15:02:53 -0400

    Added exit conditions

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	void (*iterator)(int);
 	size_t counter;
 
-	if (size == '0')
+	if (size == '0' || *array == NULL)
 		exit(1);
 
 	iterator = action;
