47c88a3d05b5b98e498241d22fc1bdb22a81c23e
OrangeB0lt <667@holbertonschool.com>
2019-03-13 15:22:43 -0400

    First commit of function 2, int index

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - search for ints
+ * @array: array to search in
+ * @size: size of array
+ * @cmp: pointer to function used to compare values
+ * Return: returns the index of the first element in which cmp function
+ * does not return 0, if no element return -1, size less than 0 return -1
+ **/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int (*search)(int);
+	int i, value
+
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	search = cmp;
+	for (i = 0; i < size; i++)
+	{
+		value = search(array[i]);
+		if (value != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
