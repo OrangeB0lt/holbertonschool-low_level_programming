bdddb770cc4e2af541475d4f2db6db39e8a8cfe0
OrangeB0lt <667@holbertonschool.com>
2019-03-13 21:42:48 -0400

    First commit of opcode printer

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * opcodes - print the opcodes
+ * @address: address of the main function
+ * @num: number of bytes to print
+ * Return: void
+ */
+void opcodes(char *address, int num)
+{
+	int i;
+
+	for (i = 0; i < num; i++)
+	{
+		printf("%02hhx", address[i]);
+		if (i < num - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+}
+
+/**
+ * main - prints opcodes of main function
+ * @argc: number of args passed in
+ * @argv: array of pointers to arguments
+ * Return: returns O
+ */
+int main(int argc, char *argv[])
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
+	return (0);
+}
