3ea1ddfb9b3ceab05911f7e12cdc293a528600b9
OrangeB0lt <667@holbertonschool.com>
2019-03-13 20:01:10 -0400

    First commit for calculator project

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -28,6 +28,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
+	if (f == '/' || f == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	answer = f(a, b);
 	printf("%d\n", answer);
 	return (0);
