f10fb597d02a5b917733bdcbfe6120f7ad9ca185
OrangeB0lt <667@holbertonschool.com>
2019-03-15 17:47:05 -0400

    first commit of print numbers

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints out numbers
+ * @separator: string to seperate numbers
+ * @n: number of arguements
+ * Return: returns nothing
+ **/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int i;
+
+	va_start(num, n);
+	for (i = 0; i< n; i++)
+	{
+		printf("%d", va_arg(num, unsigned int));
+		if (i < (n - 1) && separator != NULL)
+		{
+			printf("%s", seperator);
+		}
+	}
+	va_end(num);
+	printf("\n");
+}
