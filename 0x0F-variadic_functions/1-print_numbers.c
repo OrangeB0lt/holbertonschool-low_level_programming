58b78cd839794d85664134b67c17c61400a205c5
OrangeB0lt <667@holbertonschool.com>
2019-03-15 19:26:31 -0400

    Fixed spacing errors

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(num, n);
-	for (i = 0; i< n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(num, int));
 		if (i < (n - 1) && separator != NULL)
