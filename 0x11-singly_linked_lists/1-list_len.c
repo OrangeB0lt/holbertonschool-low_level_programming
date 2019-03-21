0c7605afad46c0c180879d4932904a3183e78a0b
OrangeB0lt <667@holbertonschool.com>
2019-03-21 13:51:44 -0400

    First commit of link list length

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - returns number of elements in a list
+ * @h: beginning of const list being counted
+ * Return: returns num of elements in a list
+ **/
+
+size_t list_len(const list_t *h)
+{
+	int index;
+	const list_t *pointer;
+
+	for(index = 0, pointer = h; pointer != NULL; index++)
+		pointer = pointer->next;
+	return(index);
+}
