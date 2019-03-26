f441b527b74e639c585042f7e870dd14a48a0193
OrangeB0lt <667@holbertonschool.com>
2019-03-26 11:54:16 -0400

    Fixed prototype

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -7,13 +7,13 @@
  * Return: returns the indexed node
  **/
 
-listint_t *get_nodeint_at_index(listint_t *head, unsigned in index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *storage;
 	unsigned int count;
 
-	for (count = 0, storage = head; count < index
-		     && storage != NULL; count++)
+	for (count = 0, storage = head; count < index && storage != NULL;
+	     count++)
 		storage = storage->next;
 	if (count != index)
 		return (NULL);
