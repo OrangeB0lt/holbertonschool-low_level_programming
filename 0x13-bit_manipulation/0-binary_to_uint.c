295ad3fa284ae6b227ec88fdc593c69b16d279b2
OrangeB0lt <667@holbertonschool.com>
2019-03-28 14:45:22 -0400

    Fixed directory name

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts binary num as an unsigned int
+ * @b: pointer to string with binary nums
+ * Return: unsigned int with decimal value of binary number, or 0 if fail
+ **/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int number;
+	int index;
+
+	number = 0;
+	if (b == NULL)
+		return (0);
+	for (index = 0; b[index] != '\0'; index++)
+	{
+		if(b[index] != '0' && b[index] != '1')
+			return (0);
+	}
+	for (index = 0; b[index] != '\0'; index++)
+	{
+		number <<= 1;
+		if (b[index] == '1')
+		{
+			number = number + 1;
+		}
+	}
+	return (number);
+}
---
+++ 0x13-bit_manipulation/README.md
@@ -0,0 +1,3 @@
+<h1> Project 0x13 <\h1>
+<br>
+This project is about working with bitwise functions
