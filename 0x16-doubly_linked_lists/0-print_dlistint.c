1c21874f789bf4fc02a4ec8ae6eda3c39324354e
OrangeB0lt <667@holbertonschool.com>
2019-05-05 15:42:59 -0400

    Spelling errors

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * print_distint - prints out double linked list
+ * print_dlistint - prints out double linked list
  * @h: head of linked list
  * Return: the num of nodes
  **/
 
-size_t print_dlistint(const distint_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t index;
 
-	for (index = 0; h != '\0'; index++)
+	for (index = 0; h != NULL; index++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
