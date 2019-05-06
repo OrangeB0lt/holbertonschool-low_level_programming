aad9e23dcbd55fe569af0b080c8ce30084406955
OrangeB0lt <667@holbertonschool.com>
2019-05-05 21:22:19 -0400

    inserts node at given point

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- *
- *
- *
- *
+ * insert_dnodeint_at_index - insters a new node at index
+ * @h: header to linked list
+ * @idx: given index
+ * @n: stuff to put in node
+ * Return: returns pointer to new node
  **/
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
@@ -13,34 +14,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *add, *nxt, *pointer;
 
 	if (h == NULL)
-	{
 		return (NULL);
-	}
 	else if (idx != 0)
 	{
 		pointer = *h;
 		count = 0;
-		while (count < idx -1 && pointer != NULL)
+		while (count < idx - 1 && pointer != NULL)
 		{
 			pointer = pointer->next;
 			count++;
 		}
 		if (pointer == NULL)
-		{
 			return (NULL);
-		}
 	}
 	add = malloc(sizeof(dlistint_t));
 	if (add == NULL)
-	{
 		return (NULL);
-	}
 	add->n = n;
 	if (idx == 0)
 	{
-		 nxt = *h;
-		 *h = add;
-		 add->prev = NULL;
+		nxt = *h;
+		*h = add;
+		add->prev = NULL;
 	}
 	else
 	{
