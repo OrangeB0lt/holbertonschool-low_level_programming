03b69eaddc3bd5b4695af48dc97e779f237e5ee8
OrangeB0lt <667@holbertonschool.com>
2019-06-02 20:54:41 -0400

    initial commit

---
+++ 0x19-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - returns hash for given string with djb2 algorithm
+ * @str: string to hash
+ * Return: hash num
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash;
+    int idx;
+
+    hash = 5381;
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + idx;
+    }
+    return (hash);
+}
