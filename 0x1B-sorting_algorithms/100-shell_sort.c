560817de91c3aad8165abdf297dca24f5ef71f65
OrangeB0lt <667@holbertonschool.com>
2019-06-17 13:39:20 -0400

    Shell sort algo

---
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * shell_sort - function to sort array of ints in ascending order with shell 
+ * sort algo
+ * @array: array
+ * @size: size of array
+ * Desc: jump * 3 + 1
+ * 
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	int storage, flag;
+	size_t idx, count, jump;
+
+	if (array == NULL || size < 1)
+		return;
+	for (jump = 1; jump < size / 3; jump = jump * 3 + 1)
+		{
+		}
+	for (; jump > 0; jump = jump / 3)
+	{
+		flag = 0;
+		for (idx = jump; idx < size; idx++)
+		{
+			storage = array[idx];
+			for (count = idx; count >= jump && array[count - jump] > storage; count -= jump)
+			{
+				array[count] = array[count - jump];
+			}
+			array[count] = storage;
+			flag = 1;
+		}
+		if (flag == 1)
+			print_array(array, size);
+	}
+
+}
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -28,6 +28,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quikSort(int *array, size_t size, int begin, int end);
+void shell_sort(int *array, size_t size);
 
 /* helper functions */
 void Aswap(int **arr, int idx_a, int idx_b);
