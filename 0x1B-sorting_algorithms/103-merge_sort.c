34163c4cb0547a2814ad741d6189d1186a7c9a14
OrangeB0lt <667@holbertonschool.com>
2019-06-18 22:22:19 -0400

    merge_sort broken

---
+++ 0x1B-sorting_algorithms/103-merge_sort.c
@@ -0,0 +1,83 @@
+#include "sort.h"
+/**
+ * 
+ * 
+ * 
+ */
+void merge_sort(int *array, size_t size)
+{
+	int *covfefe;
+
+	if (array && size > 1)
+	{
+		covfefe = malloc(sizeof(int *) * size);
+		_sort(0, size - 1, covfefe, array);
+		free(covfefe);
+	}
+
+}
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+void _sort(int begin, int end, int *covfefe, int *array)
+{
+	int s;
+
+	if (begin < end)
+	{
+		s = (begin + end) / 2;
+		if ((end + begin) % 2 == 0)
+			s--;
+		_sort(begin, s, covfefe, array);
+		_sort(s + 1, end, covfefe, array);
+		merge(begin, end, covfefe, array);
+	}
+}
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+void merge(int begin, int end, int *covfefe, int *array)
+{
+	int a, d, s, storage, storeSize;
+
+	storeSize = end - begin + 1;
+	s = ((begin + end) / 2);
+	if ((begin + end) % 2 == 1)
+		s++;
+
+	/* printing stuff */
+	printf("Merging...\n");
+	a = begin;
+	printf("[left]: ");
+	print_array(array + a, (size_t)(s - a));
+	d = s;
+	printf("[right]: ");
+	print_array(array + d, (size_t)((end - s) + 1));
+
+	storage = 0;
+	while (a < s && d <= end)
+	{
+		if (array[a] < array[d])
+			covfefe[storage++] = array[a++];
+		else
+			covfefe[storage] = array[d++];
+	}
+	while (a < s)
+		covfefe[storage++] = array[a++];
+	while (d <= end)
+		covfefe[storage++] = array[d++];
+	
+	storage = 0;
+	a = begin;
+	while (storage < storeSize)
+		array[a++] = covfefe[storage++];
+
+	printf("[Done]: ");
+	print_array(array + begin, storeSize);
+}
