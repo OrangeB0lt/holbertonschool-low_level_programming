9e114cc1968c29aec7d1a474fa0493a6f5d250f5
OrangeB0lt <667@holbertonschool.com>
2019-06-19 20:02:29 -0400

    cocktail

---
+++ 0x1B-sorting_algorithms/101-cocktail_sort_list.c
@@ -0,0 +1,73 @@
+#include "sort.h"
+
+/**
+ * Cswap - swaps a node with next node in list
+ * @head: double pointer to start of list
+ * @node: node
+ */ 
+void Cswap(listint_t **head, listint_t *node)
+{
+	node->next->prev = node->prev;
+	if (node->prev)
+		node->prev->next = node->next;
+	else
+	{
+		*head = node->next;
+	}
+	node->prev = node->next;
+	node->next = node->next->next;
+	node->prev->next = node;
+	if (node->next)
+		node->next->prev = node;
+}
+
+/**
+ * cocktail_sort_list - sorts a doubly linked list using cocktail sort algo
+ * @list: pointer to head of dlinked list
+ */
+
+void cocktail_sort_list(listint_t **list)
+{
+	char replace;
+	listint_t *storage;
+
+	replace = 1;
+
+	if (*list == NULL || list == NULL)
+		return;
+
+	storage = *list;
+	
+	while (replace != 0)
+	{
+		replace = 0;
+		while (storage->next->n < storage->n)
+		{
+			if (storage->next->n < storage->n)
+			{
+				Cswap(list, storage);
+				replace = 1;
+				print_list(*list);	
+			}
+			else
+			{
+				storage = storage->next;
+			}
+		}
+		if (replace == 0)
+			break;
+		replace = 0;
+		while (storage->prev != NULL)
+		{
+			if (storage->prev->n > storage->n)
+			{
+				Cswap(list, storage->prev);
+				replace = 1;
+				print_list(*list);
+			}
+			else
+				storage = storage->prev;
+		}
+			
+	}
+}
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -27,9 +27,11 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 void quikSort(int *array, size_t size, int begin, int end);
 void shell_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 /* helper functions */
 void Aswap(int **arr, int idx_a, int idx_b);
@@ -38,6 +40,7 @@ void Bswap(int *array, size_t size, int second, int third);
 int parti(int *array, size_t size, int begin, int turn);
 void merge(int begin, int end, int *covfefe, int *array);
 void _sort(int begin, int end, int *covfefe, int *array);
+void Cswap(listint_t **head, listint_t *node);
 
 
 #endif
