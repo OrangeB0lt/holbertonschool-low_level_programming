b03b405c9aca7730c943b0057593791823ac2db9
OrangeB0lt <667@holbertonschool.com>
2019-07-23 16:54:45 -0400

    init commit for tree

---
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a node to the right in tree
+ * @parent: pointer to parent node
+ * @value: vlaue of new node
+ * Return: returns new node or NULL
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *rightNode;
+
+    if (parent == NULL)
+        return (NULL);
+
+    rightNode = binary_tree_node(parent, value);
+    if (rightNode == NULL)
+        return (NULL);
+    if (parent->right != NULL)
+    {
+        rightNode->right = parent->right;
+        rightNode->right->parent = rightNode;
+    }
+    parent->right = rightNode;
+    return (rightNode);
+}
